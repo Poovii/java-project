5fa2fcb6ef38e13640db52f93459d02b70a20fac
D <104251994+Poovii@users.noreply.github.com>
2022-10-29 02:52:46 -0400

    Add files via upload
    It is collection of Simple java Programs

---
+++ collection of program/even.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class even{
+    public static void main(String[] args) {
+        //int a=37;
+       
+        //if(a%2==0){
+            //System.out.println("a is even number");
+        //}
+
+        //else{ 
+            //System.out.println("a is odd");
+        //}
+        
+        }
+
+    
+        }
+    }
---
+++ collection of program/myprogm.java
@@ -0,0 +1,24 @@
+public class myprogm{
+    public static void main(String[] args){
+        for(int i=0;i<=100;i++){
+            if(i%2!=0){
+            System.out.println(i);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
